2996c823fec8ef570d8db4f76d04ed683f5df099
Arnab <iamarnabbiswas2001@gmail.com>
2024-01-09 11:28:22 +0530

    approver

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,7 +4,9 @@ const reviewerSignup=require('../controller/reviewerSignup')
 const reviewerlogin=require('../controller/reviewerLogin');
 const applicantSignup=require('../controller/applicantSignup')
 const applicantlogin=require('../controller/applicantLogin')
-
+const allReviewerOfProvince=require('../controller/allReviewerOfProvince')
+const approverSignup=require('../controller/approverSignup')
+const approverlogin=require('../controller/approverLogin')
 
 router.post('/reviewersignup',reviewerSignup)
 
@@ -13,6 +15,12 @@ router.post('/reviewerlogin',reviewerlogin)
 router.post('/applicantsignup',applicantSignup)
 
 router.post('/applicantlogin',applicantlogin)
+
+router.post('/approversignup',approverSignup);
+
+router.get('/allreviewer/:province',allReviewerOfProvince);
+
+router.post('/approverlogin',approverlogin)
 
 
 
