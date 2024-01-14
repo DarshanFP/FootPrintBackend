8e826794eb038b53ed8293f365ada2f4c9ac2577
Arnab <iamarnabbiswas2001@gmail.com>
2024-01-14 18:08:14 +0530

    password changes

--- controller/applicantChangepassword.js
+++ controller/applicantChangepassword.js
@@ -1,23 +1,22 @@
 const Applicant = require("../modals/Applicant");
-const bcrypt=require('bcrypt');
+const bcrypt = require("bcrypt");
 
-const applicantChangePassword=async(req,res)=>{
+const applicantChangePassword = async (req, res) => {
     try {
-        const {newpassword}=req.body
-        if(!newpassword){
-            return res.json({success:false, msg:'Enter new password'})
+        const { newpassword } = req.body;
+        if (!newpassword) {
+            return res.json({ success: false, msg: "Enter new password" });
         }
         const salt = await bcrypt.genSalt(10);
         const securepassword = await bcrypt.hash(newpassword, salt);
 
-        await Applicant.findByIdAndUpdate(req.user.id,{
-            password:securepassword
-        })
-        return res.json({success:true})
-
+        await Applicant.findByIdAndUpdate(req.user.id, {
+            password: securepassword,
+        });
+        return res.json({ success: true });
     } catch (error) {
         console.log(error);
         res.json({ success: false });
     }
-}
-module.exports=applicantChangePassword
+};
+module.exports = applicantChangePassword;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,71 +1,68 @@
-const express = require('express');
-const router = express.Router()
-const reviewerSignup=require('../controller/reviewerSignup')
-const reviewerlogin=require('../controller/reviewerLogin');
-const applicantSignup=require('../controller/applicantSignup')
-const applicantlogin=require('../controller/applicantLogin')
-const allReviewerOfProvince=require('../controller/allReviewerOfProvince')
-const approverSignup=require('../controller/approverSignup')
-const approverlogin=require('../controller/approverLogin')
-const reviewerVarify=require('../controller/reviewerVarify')
-const reviewerUnvarify=require('../controller/reviewerUnvarify')
-const applicantVarify=require('../controller/applicantVarify')
-const applicantUnvarify=require('../controller/applicantUnvarify')
-const allreviewer=require('../controller/allReviewer')
-const allapplicant=require('../controller/allApplicant')
-const {protectReviewer}= require('../middleware/authReviewer')
-const {protectApprover}=require('../middleware/authApprover')
-const {protectApplicant}=require('../middleware/authApplicant');
-const reviewerChangePassword=require('../controller/reviewerChangePassword')
-const getApplicant=require('../controller/getApplicant')
-const getReviewer=require('../controller/getReviewer')
-const getApprover=require('../controller/getApprover')
-const approverChangePassword=require('../controller/approverChangePassword')
-const applicantChangePassword=require('../controller/applicantChangepassword')
+const express = require("express");
+const router = express.Router();
+const reviewerSignup = require("../controller/reviewerSignup");
+const reviewerlogin = require("../controller/reviewerLogin");
+const applicantSignup = require("../controller/applicantSignup");
+const applicantlogin = require("../controller/applicantLogin");
+const allReviewerOfProvince = require("../controller/allReviewerOfProvince");
+const approverSignup = require("../controller/approverSignup");
+const approverlogin = require("../controller/approverLogin");
+const reviewerVarify = require("../controller/reviewerVarify");
+const reviewerUnvarify = require("../controller/reviewerUnvarify");
+const applicantVarify = require("../controller/applicantVarify");
+const applicantUnvarify = require("../controller/applicantUnvarify");
+const allreviewer = require("../controller/allReviewer");
+const allapplicant = require("../controller/allApplicant");
+const { protectReviewer } = require("../middleware/authReviewer");
+const { protectApprover } = require("../middleware/authApprover");
+const { protectApplicant } = require("../middleware/authApplicant");
+const reviewerChangePassword = require("../controller/reviewerChangePassword");
+const getApplicant = require("../controller/getApplicant");
+const getReviewer = require("../controller/getReviewer");
+const getApprover = require("../controller/getApprover");
+const approverChangePassword = require("../controller/approverChangePassword");
+const applicantChangePassword = require("../controller/applicantChangepassword");
 
+router.post("/reviewersignup", reviewerSignup);
 
-router.post('/reviewersignup',reviewerSignup)
+router.post("/reviewerlogin", reviewerlogin);
 
-router.post('/reviewerlogin',reviewerlogin)
+router.post("/applicantsignup", applicantSignup);
 
-router.post('/applicantsignup',applicantSignup)
+router.post("/applicantlogin", applicantlogin);
 
-router.post('/applicantlogin',applicantlogin)
+router.post("/approversignup", approverSignup);
 
-router.post('/approversignup',approverSignup);
+router.get("/allreviewer/:province", allReviewerOfProvince);
 
-router.get('/allreviewer/:province',allReviewerOfProvince);
+router.post("/approverlogin", approverlogin);
 
-router.post('/approverlogin',approverlogin)
+router.put("/reviewervarify", protectApprover, reviewerVarify);
 
-router.put('/reviewervarify',protectApprover,reviewerVarify)
+router.put("/applicantvarify", protectReviewer, applicantVarify);
 
-router.put('/applicantvarify',protectReviewer,applicantVarify)
+router.delete("/reviewerunvarify", protectApprover, reviewerUnvarify);
 
-router.delete('/reviewerunvarify',protectApprover,reviewerUnvarify)
+router.delete("/applicantunvarify", protectReviewer, applicantUnvarify);
 
-router.delete('/applicantunvarify',protectReviewer,applicantUnvarify)
+router.get("/allreviewer", protectApprover, allreviewer);
 
-router.get('/allreviewer',protectApprover,allreviewer)
+router.get("/allapplicant", protectReviewer, allapplicant);
 
-router.get('/allapplicant',protectReviewer,allapplicant)
+router.get("/getapplicant", protectApplicant, getApplicant);
 
-router.get('/getapplicant',protectApplicant,getApplicant);
+router.get("/getreviewer", protectReviewer, getReviewer);
 
-router.get('/getreviewer',protectReviewer,getReviewer)
+router.get("/getapprover", protectApprover, getApprover);
 
-router.get('/getapprover',protectApprover,getApprover)
+router.put("/changepasswordreviewer", protectReviewer, reviewerChangePassword);
 
-router.put('/changepasswordreviewer',protectReviewer,reviewerChangePassword)
+router.put("/changepasswordapprover", protectApprover, approverChangePassword);
 
-router.put('/changepasswordapprover',protectApprover,approverChangePassword)
+router.put(
+  "/changepasswordapplicant",
+  protectApplicant,
+  applicantChangePassword
+);
 
-router.put('/changepasswordapplicant',protectApplicant,applicantChangePassword)
-
-
-
-
-module.exports=router
-
-
-
+module.exports = router;
